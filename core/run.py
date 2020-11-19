6088b7860e9dee566337e33c18a2b3fc453eec83
UtkuY <utkuyucel35@gmail.com>
2020-11-19 16:57:52 +0300

    main app

---
+++ core/run.py
@@ -0,0 +1,12 @@
+from check import Checker
+
+books = {
+	"borsa sihirbazları":"https://www.idefix.com/Kitap/Borsa-Sihirbazlari/Egitim-Basvuru/Is-Ekonomi-Hukuk/Borsa/urunno=0000000066921",
+	"bir borsa spekülatörünün anıları": "https://www.idefix.com/Kitap/Bir-Borsa-Spekulatorunun-Anilari/Egitim-Basvuru/Is-Ekonomi-Hukuk/Borsa/urunno=0000000066918",
+	"trend takipçisi":"https://www.idefix.com/Kitap/Trend-Takipcisi/Egitim-Basvuru/Is-Ekonomi-Hukuk/Yonetim-Is-Gelistirme-Kalite/urunno=0000000288327",
+	"sıradan hisseler sıradışı karlar":"https://www.idefix.com/Kitap/Siradan-Hisseler-Siradisi-Karlar/Egitim-Basvuru/Is-Ekonomi-Hukuk/Borsa/urunno=0000000229324",
+	"büyük yatırımcılar":"https://www.idefix.com/Kitap/Buyuk-Yatirimcilar/Egitim-Basvuru/Is-Ekonomi-Hukuk/Yonetim-Is-Gelistirme-Kalite/urunno=0000000576986",
+}
+
+checker = Checker(books)
+
